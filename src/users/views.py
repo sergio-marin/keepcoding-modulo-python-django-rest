9daea21c9364883274d0d5ffc0ab604cd7f55775
Sergio Marin <serjey@gmail.com>
2017-04-16 23:26:40 +0200

    Se cambia el formulario de login a Django Forms

--- src/users/views.py
+++ src/users/views.py
@@ -2,6 +2,8 @@ from django.contrib.auth import authenticate, login as django_login, logout as d
 from django.shortcuts import render, redirect
 from django.views import View
 
+from users.forms import LoginForm
+
 
 class LoginView(View):
 
@@ -11,7 +13,10 @@ class LoginView(View):
         :param request:  HttpRequest
         :return: HttpResponse
         """
-        return render(request, 'login.html')
+        context = {
+            'form': LoginForm()
+        }
+        return render(request, 'login.html', context)
 
     def post(self, request):
         """
@@ -19,19 +24,21 @@ class LoginView(View):
         :param request:  HttpRequest
         :return: HttpResponse
         """
+        form = LoginForm(request.POST)
         context = dict()
-        username = request.POST.get("usr")
-        password = request.POST.get("pwd")
-        user = authenticate(username=username, password=password)
-        if user is not None:
-            # usuario autenticado
-            django_login(request, user)
-            url = request.GET.get('next', 'tasks_list')
-            return redirect(url)
-        else:
-            # usuario no autenticado
-            context["error"] = "Wrong username or password."
-
+        if form.is_valid():
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                # usuario autenticado
+                django_login(request, user)
+                url = request.GET.get('next', 'tasks_list')
+                return redirect(url)
+            else:
+                # usuario no autenticado
+                context["error"] = "Wrong username or password."
+        context["form"] = form
         return render(request, 'login.html', context)
 
 
