c7240b99a2e9c8378069b25efa208232de61cfd7
Sergio Marin <serjey@gmail.com>
2017-04-17 14:30:36 +0200

    Creados endpoints para modificar, eliminar y crear usuarios

--- src/users/api.py
+++ src/users/api.py
@@ -1,4 +1,6 @@
 from django.contrib.auth.models import User
+from rest_framework import status
+from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -6,13 +8,73 @@ from users.serializers import UserSerializer
 
 
 class UsersAPI(APIView):
+    """
+        Lists (GET) and creates (POST) users
+    """
 
     def get(self, request):
         """
         Returns a list of system users
-        :param request: HttpResuest
-        :return: HttpResponse
+        :param request: HttpRequest
+        :return: Response
         """
         users = User.objects.all()
         serializer = UserSerializer(users, many=True)
         return Response(serializer.data)
+
+    def post(self, request):
+        """
+        Creates a user
+        :param request: HttpRequest
+        :return: Response
+        """
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserDetailAPI(APIView):
+    """
+    User detail (GET), update user (PUT), delete user (DELETE)
+    """
+
+    def get(self, request):
+        """
+        Returns a requested user
+        :param request: HttpRequest
+        :param pk: user primary key
+        :return: Response
+        """
+        user = get_object_or_404(User, pk=pk)
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        """
+        Updates a User with the given data
+        :param request: HttpRequest
+        :param pk: User primary key
+        :return: Response
+        """
+        user = get_object_or_404(User, pk=pk)
+        serializer = UserSerializer(user, data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+    def delete(self, request, pk):
+        """
+        Deletes a user
+        :param request:
+        :param pk: User Primary Key
+        :return: Response
+        """
+        user = get_object_or_404(User, pk=pk)
+        user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
--- src/users/serializers.py
+++ src/users/serializers.py
@@ -1,15 +1,25 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 
 
 class UserSerializer(serializers.Serializer):
 
-    id = serializers.CharField()
+    id = serializers.ReadOnlyField()
     username = serializers.CharField()
     first_name = serializers.CharField()
     last_name = serializers.CharField()
+    email = serializers.EmailField()
+    password = serializers.CharField()
 
     def create(self, validated_data):
-        pass
+        return self.update(User(), validated_data)
 
     def update(self, instance, validated_data):
-        pass
+        instance.first_name = validated_data.get('first_name', instance.first_name)
+        instance.last_name = validated_data.get('last_name', instance.last_name)
+        instance.username = validated_data.get('username', instance.username)
+        instance.email = validated_data.get('email', instance.email)
+        if validated_data.get('password'):
+            instance.set_password(validated_data.get('password'))
+        instance.save()
+        return instance
