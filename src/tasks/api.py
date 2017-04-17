d594f9df20f81f82a76125492e14170ab84070af
Sergio Marin <serjey@gmail.com>
2017-04-17 21:00:47 +0200

    Se controla que se asigne una tarea a quien la ha creado desde la API

--- src/tasks/api.py
+++ src/tasks/api.py
@@ -15,6 +15,9 @@ class TasksAPI(ListCreateAPIView):
     def get_serializer_class(self):
         return TaskListSerializer if self.request.method == "GET" else TaskSerializer
 
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
 
 class TaskDetailAPI(RetrieveUpdateDestroyAPIView):
     """
