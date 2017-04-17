cf88dbac92337b354bace18382d2083ae0f5d01e
Sergio Marin <serjey@gmail.com>
2017-04-17 21:38:38 +0200

    Pasamos a ViewSet el API de las Tareas

--- src/tasks/api.py
+++ src/tasks/api.py
@@ -1,28 +1,17 @@
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import ModelViewSet
 
 from tasks.models import Task
 from tasks.serializers import TaskSerializer, TaskListSerializer
 
 
-class TasksAPI(ListCreateAPIView):
-    """
-    Lists (GET) and creates (POST) tasks
-    """
+class TaskViewSet(ModelViewSet):
+
     queryset = Task.objects.all()
     permission_classes = (IsAuthenticated, )
 
     def get_serializer_class(self):
-        return TaskListSerializer if self.request.method == "GET" else TaskSerializer
+        return TaskListSerializer if self.action == "list" else TaskSerializer
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
-
-
-class TaskDetailAPI(RetrieveUpdateDestroyAPIView):
-    """
-    Retrieves (GET), updates (PUT) and destroy (DELETE) a given task
-    """
-    queryset = Task.objects.all()
-    serializer_class = TaskSerializer
-    permission_classes = (IsAuthenticated,)
