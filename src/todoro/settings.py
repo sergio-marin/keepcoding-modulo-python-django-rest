9bc8ec630e819889fb0636477aba41070716f322
Sergio Marin <serjey@gmail.com>
2017-04-18 19:04:31 +0200

    Se añaden varios Renderer para poder devolver resultados en diferentes formatos

--- src/todoro/settings.py
+++ src/todoro/settings.py
@@ -152,5 +152,12 @@ LOGIN_URL = '/login'
 # REST Framework Settings
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 3
+    'PAGE_SIZE': 3,
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+        'rest_framework_xml.renderers.XMLRenderer',
+        'rest_framework_csv.renderers.CSVRenderer',
+        'rest_framework_yaml.renderers.YAMLRenderer'
+    )
 }
